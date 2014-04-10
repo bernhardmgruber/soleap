845ee0d89b5db1181bfe71bfdcfb9f2498c4610f
dixxi <dixxi1@gmx.net>
2014-04-10 03:19:04 +0200

    damn, committed some source files with last commit. well, here is the rest. basic implementation of leap data viewer using SharpGL and Winms

--- src/LeapHandReconstruction/Program.cs
+++ src/LeapHandReconstruction/Program.cs
@@ -1,16 +1,47 @@
-﻿using System;
+﻿using Leap;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace LeapHandReconstruction
 {
     class Program
     {
+        private static LeapViewer viewer;
+
+        [STAThread]
         static void Main(string[] args)
         {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
 
+            using (var leap = new LeapMotionDevice())
+            {
+                viewer = new LeapViewer();
+                leap.OnFrame += leap_OnFrame;
+                Application.Run(viewer);
+            }
+        }
+
+        static void leap_OnFrame(Frame frame)
+        {
+            viewer.Invoke(new Action(() =>
+            {
+                Console.Clear();
+                Console.WriteLine("Frame");
+                foreach (var hand in frame.Hands)
+                {
+                    Console.WriteLine("    Hand");
+                    foreach (var finger in hand.Fingers)
+                        Console.WriteLine("        Finger " + finger.TipPosition.ToString());
+                }
+
+                viewer.DrawFrame(frame);
+            }));
         }
     }
 }
