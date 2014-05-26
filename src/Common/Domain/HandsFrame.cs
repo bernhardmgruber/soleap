8101e43822e47e4ca70f2b90922d68ac7a349784
dixxi <dixxi1@gmx.net>
2014-05-26 23:31:43 +0200

    added Common project and started with domain HandFrame classes

---
+++ src/Common/Domain/HandsFrame.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Media3D;
+
+namespace Common.Domain
+{
+    public class Finger
+    {
+
+    }
+
+    public class Hand
+    {
+        public Vector3D HandDirection { get; set; }
+
+        public Point3D PalmPosition { get; set; }
+
+        public Vector3D PalmNormal { get; set; }
+
+        public IList<Finger> Fingers { get; set; }
+    }
+
+    public class HandsFrame
+    {
+        IList<Hand> Hands { get; set; }
+    }
+}
---
+++ src/Common/Interfaces/IHandFrameProvider.cs
@@ -0,0 +1,16 @@
+﻿using Common.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.Interfaces
+{
+    public delegate void HandFrameReadHandler(HandsFrame frame);
+
+    public interface IHandFrameProvider
+    {
+        event HandFrameReadHandler FrameReady;
+    }
+}
